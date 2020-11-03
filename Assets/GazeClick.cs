8a7a0e4a0083d34e3016fa33ca6a4123decab503
krystianplata <236250@student.pwr.edu.pl>
2020-11-03 20:31:28 +0100

    [UI] Created main menu canvas
    This commit introduces the main menu element, with a "gaze click"
    handler for a start button.
    
    Imported assets:
        - Text Mesh Pro
        - Clean Vector Icons

---
+++ Assets/GazeClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class GazeClick : MonoBehaviour
+{
+    public float gazeTime = .5f;
+    private float timer;
+    private bool gazedAt;
+
+    void Update() {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+                timer = 0f;
+            }
+        }
+    }
+
+    public void PointerEnter()
+    {
+        gazedAt = true;
+    }
+
+    public void PointerExit()
+    {
+        timer = 0f;
+        gazedAt = false;
+    }
+
+    public void PointerClick()
+    {
+        // placeholder action
+        Debug.Log("Button has been clicked");
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+}
