c9f56a00920d30d9c7d85b842d02a473d2067989
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2020-11-17 22:36:11 +0100

    done movement demo and collision

---
+++ Assets/BoxMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BoxMovement : MonoBehaviour
+{
+    public GameObject Player;
+
+    public int obstacleSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = new Vector3(0,0,1) * obstacleSpeed * Time.deltaTime;
+        transform.Translate(dir.x, dir.y, dir.z);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == Player.name)
+        {
+            transform.Translate(3, 2, 4);
+        }
+    }
+}
