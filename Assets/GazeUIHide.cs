5e960452516ccf6aa85e22f2b10281aa140f5f5e
krystianplata <236250@student.pwr.edu.pl>
2020-11-21 17:17:36 +0100

    [UI] Remove whole UI panel on gaze click

--- Assets/GazeUIHide.cs
+++ Assets/GazeUIHide.cs
@@ -1,5 +1,5 @@
-//-----------------------------------------------------------------------
-// <copyright file="ObjectController.cs" company="Google LLC">
+﻿//-----------------------------------------------------------------------
+// <copyright file="GazeUIHide.cs" company="Google LLC">
 // Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,7 +23,7 @@ using UnityEngine;
 /// <summary>
 /// Controls target objects behaviour.
 /// </summary>
-public class ObjectController : MonoBehaviour
+public class GazeUIHide : MonoBehaviour
 {
     /// <summary>
     /// The material to use when this object is inactive (not being gazed at).
@@ -34,33 +34,21 @@ public class ObjectController : MonoBehaviour
     /// The material to use when this object is active (gazed at).
     /// </summary>
     public Material GazedAtMaterial;
-       
+
     public float gazeTime = .5f;
     private float timer;
     private bool gazedAt;
-
-    // The objects are about 1 meter in radius, so the min/max target distance are
-    // set so that the objects are always within the room (which is about 5 meters
-    // across).
-    private const float _minObjectDistance = 2.5f;
-    private const float _maxObjectDistance = 3.5f;
-    private const float _minObjectHeight = 0.5f;
-    private const float _maxObjectHeight = 3.5f;
-
-    private Renderer _myRenderer;
-    private Vector3 _startingPosition;
+    private GameObject _UI;
 
     /// <summary>
     /// Start is called before the first frame update.
     /// </summary>
     public void Start()
     {
-        _startingPosition = transform.localPosition;
-        _myRenderer = GetComponent<Renderer>();
-        SetMaterial(false);
+        _UI = GameObject.Find("UI_Plane");
     }
 
-    void Update()
+    public void Update()
     {
         if (gazedAt)
         {
@@ -77,34 +65,33 @@ public class ObjectController : MonoBehaviour
     /// <summary>
     /// Teleports this instance randomly when triggered by a pointer click.
     /// </summary>
-    public void TeleportRandomly()
-    {
+    //public void TeleportRandomly()
+    //{
         // Picks a random sibling, moves them somewhere random, activates them,
         // deactivates ourself.
-        int sibIdx = transform.GetSiblingIndex();
-        int numSibs = transform.parent.childCount;
-        sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-        GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
-
-        // New object's location.
-        float angle = Random.Range(-90, 90);
-        float distance = Random.Range(_minObjectDistance, _maxObjectDistance);
-        float height = Random.Range(_minObjectHeight, _maxObjectHeight);
-        Vector3 newPos = new Vector3(Mathf.Cos(angle) * distance, height,
-                                     Mathf.Sin(angle) * distance);
-        randomSib.transform.localPosition = newPos;
-
-        randomSib.SetActive(true);
-        gameObject.SetActive(false);
-        SetMaterial(false);
-    }
+        // int sibIdx = transform.GetSiblingIndex();
+        // int numSibs = transform.parent.childCount;
+        // sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+        // GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+        // 
+        // // New object's location.
+        // float angle = Random.Range(-90, 90);
+        // float distance = Random.Range(_minObjectDistance, _maxObjectDistance);
+        // float height = Random.Range(_minObjectHeight, _maxObjectHeight);
+        // Vector3 newPos = new Vector3(Mathf.Cos(angle) * distance, height,
+        //                              Mathf.Sin(angle) * distance);
+        // randomSib.transform.localPosition = newPos;
+        // 
+        // randomSib.SetActive(true);
+        // gameObject.SetActive(false);
+        // SetMaterial(false);
+    //}
 
     /// <summary>
     /// This method is called by the Main Camera when it starts gazing at this GameObject.
     /// </summary>
     public void OnPointerEnter()
     {
-        SetMaterial(true);
         gazedAt = true;
     }
 
@@ -112,8 +99,7 @@ public class ObjectController : MonoBehaviour
     /// This method is called by the Main Camera when it stops gazing at this GameObject.
     /// </summary>
     public void OnPointerExit()
-    {
-        SetMaterial(false);
+    { 
         gazedAt = false;
     }
 
@@ -123,21 +109,6 @@ public class ObjectController : MonoBehaviour
     /// </summary>
     public void OnPointerClick()
     {
-        TeleportRandomly();
-    }
-
-    /// <summary>
-    /// Sets this instance's material according to gazedAt status.
-    /// </summary>
-    ///
-    /// <param name="gazedAt">
-    /// Value `true` if this object is being gazed at, `false` otherwise.
-    /// </param>
-    private void SetMaterial(bool gazedAt)
-    {
-        if (InactiveMaterial != null && GazedAtMaterial != null)
-        {
-            _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
-        }
+        _UI.transform.position = new Vector3(100, 100, 100);
     }
 }
