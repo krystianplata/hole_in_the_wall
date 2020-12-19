c968c37380a76d2d59c5e1fc2251b8f89ab45f56
krystianplata <236250@student.pwr.edu.pl>
2020-12-19 16:48:52 +0100

    Added basic wall generator script

---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : MonoBehaviour
+{
+
+    // Public: script for collisions & material
+
+    public Vector3 spawnPoint;
+    public Vector3 spawnRotation = new Vector3(-90.0f, 0.0f, -90.0f);
+    public GameObject UI;
+
+    private List<GameObject> walls = new List<GameObject>();
+    private GameObject activeWall = null;
+
+    void Start()
+    {
+        Object[] prefabs = Resources.LoadAll("Walls");
+        foreach (var prefab in prefabs)
+        {
+            if (prefab is GameObject) {
+                walls.Add(prefab as GameObject);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!activeWall && !UI.activeSelf)
+        {
+            SpawnRandomWall();
+        }
+    }
+
+    void SpawnRandomWall()
+    {
+        activeWall = Instantiate(walls[0]) as GameObject;
+        activeWall.transform.position = spawnPoint;
+        activeWall.transform.eulerAngles = spawnRotation;
+    }
+
+    void ClearUsedWall()
+    {
+        // Add a delete for the game object
+        activeWall = null;
+    }
+}
