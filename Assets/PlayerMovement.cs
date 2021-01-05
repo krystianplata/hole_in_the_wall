fb28de546fdb31d05d85f6f2f7f713af0c056e8d
Bartosz Prokopowicz <prokopowicz@vialutions.com>
2021-01-05 14:25:53 +0100

    added stopping moving before start, added bounds

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,16 +7,40 @@ public class PlayerMovement : MonoBehaviour
     public int playerSpeed;
     public int xSentitivity;
     public int ySentitivity;
+    public GameObject ui_plane;
+    private Rigidbody rigidbody;
+    private bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
         Input.gyro.enabled = true;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Input.gyro.rotationRate * playerSpeed * Time.deltaTime;
-        transform.Translate(dir.y * xSentitivity * -1, dir.x * ySentitivity, 0);
+        if (ui_plane.activeSelf)
+        {
+            return;
+        }
+
+        if (isMoving)
+        {
+            rigidbody.freezeRotation = true;
+            Vector3 dir = Input.gyro.rotationRate * playerSpeed * Time.deltaTime;
+            // transform.Translate(dir.y * xSentitivity * -1, dir.x * ySentitivity, 0);
+            Vector3 dir2 = new Vector3(dir.y * xSentitivity * -1, dir.x * ySentitivity, 0);
+            rigidbody.velocity = dir2 * 100;
+        }
+        isMoving = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.parent.gameObject.name == "Bounds")
+        {
+            isMoving = false;
+        }
     }
 }
